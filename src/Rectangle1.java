9afb158488b7ea4b57eddabd96e9f03f2c5f8f74
Ian Imperial <ianimp96@vt.edu>
2017-09-05 02:44:31 -0400

    Added javadoc at beginning as well as potential functions to implement.

--- src/Rectangle1.java
+++ src/Rectangle1.java
@@ -2,43 +2,56 @@
 /**
  * @author ianimp96
  * @author nickeda
+ * 
+ * The Rectangle1 program reads and processes commands to handle a collection
+ * of rectangles in a database. The program allows the user to insert, delete,
+ * and perform searches on the database using the Binary Search Tree data 
+ * structure.
  *
  */
-public class Rectangle1 {
 
-	
-	// On my honor: 
-	//
-	// - I have not used source code obtained from another student,
-	// or any other unauthorized source, either modified or
-	// unmodified. 
-	//
-	// - All source code and documentation used in my program is
-	// either my original work, or was derived by me from the
-	// source code published in the textbook for this course. 
-	//
-	// - I have not discussed coding details about this project with
-	// anyone other than my partner (in the case of a joint
-	// submission), instructor, ACM/UPE tutors or the TAs assigned
-	// to this course. I understand that I may discuss the concepts
-	// of this program with other students, and that another student
-	// may help me debug my program so long as neither of us writes
-	// anything during the discussion or modifies any computer file
-	// during the discussion. I have violated neither the spirit nor
-	// letter of this restriction.
-	//
-	// Ian Imperial
-	// Nick Eda
-	
-	
+// On my honor: 
+//
+// - I have not used source code obtained from another student,
+// or any other unauthorized source, either modified or
+// unmodified. 
+//
+// - All source code and documentation used in my program is
+// either my original work, or was derived by me from the
+// source code published in the textbook for this course. 
+//
+// - I have not discussed coding details about this project with
+// anyone other than my partner (in the case of a joint
+// submission), instructor, ACM/UPE tutors or the TAs assigned
+// to this course. I understand that I may discuss the concepts
+// of this program with other students, and that another student
+// may help me debug my program so long as neither of us writes
+// anything during the discussion or modifies any computer file
+// during the discussion. I have violated neither the spirit nor
+// letter of this restriction.
+//
+// Ian Imperial
+// Nick Eda
+
+public class Rectangle1 {
 	
 	/**
 	 * @param args
-	 * 				the arguments entered
+	 * 			the arguments entered
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// invoked as java Rectangle1 {command-file}
 
 	}
+	
+	// insert name x y w h
+	// remove name
+	// remove x y w h
+	// regionsearch x y w h
+	// intersections
+	// search name
+	// dump
+	
+	
 
 }
