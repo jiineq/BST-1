2a55ff60238ddc0fe2dafa145221f1caec8f27ca
Ian Imperial <ianimp96@vt.edu>
2017-09-18 15:08:50 -0400

    We should have all 20 points for Style/Coding now

--- src/BST.java
+++ src/BST.java
@@ -350,6 +350,11 @@ public class BST<T extends Comparable<? super T>> implements Iterator<T> {
         return root;
     }
 
+    /**
+     * Getter for the size of the tree
+     * 
+     * @return the size of the tree
+     */
     public int getSize() {
         return size;
     }
